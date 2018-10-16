7647864e869724e4e472978bde7ccd1003068983
GeorgeyGigz <georgeymutti@gmail.com>
2018-10-17 00:41:39 +0300

    [Finishes #161253568]Add test cases

--- app/api/v1/views/store_views.py
+++ app/api/v1/views/store_views.py
@@ -1,4 +1,4 @@
-#app/api/v1/views/store_views.py
+# app/api/v1/views/store_views.py
 '''
 This is where all API Endpoints will be captured
 '''
@@ -6,93 +6,118 @@ from flask import request
 from datetime import date
 from flask_restful import Resource
 
-#import class products
+# import class products
 from app.api.v1.models.store_model import StoreManager
 
 
-products=StoreManager().get_all_products()
-sales_record=StoreManager().get_all_sales()
+products = StoreManager().get_all_products()
+sales_record = StoreManager().get_all_sales()
+
+
 class ViewProducts(Resource):
     '''Get all products'''
+
     def get(self):
-        return {"Available Products":products},200
-    
+        return {"Available Products": products}, 200
+
     '''Adding a new product'''
+
     def post(self):
-       
-        data=request.get_json(force=True)
-        product_id=len(products)+1
-        product_name=data["product_name"]
-        category=data["category_id"]
-        stock_amount=data["stock_amount"]
-        price=data['price']
-        product=[product for product in products if product['product_name']==request.json['product_name']]
-        if type((request.json['stock_amount']) or (request.json['price'])) not in [int,float]:
-            return {"Error":"Require int or float type"}
+
+        data = request.get_json(force=True)
+        product_id = len(products)+1
+        product_name = data["product_name"]
+        category = data["category_id"]
+        stock_amount = data["stock_amount"]
+        price = data['price']
+        product = [product for product in products if product['product_name']
+                   == request.json['product_name']]
+        if (not request.json or not "product_name" in request.json):
+            return {'Error': "Request Not found"}, 400  # not found
+
+        if type((request.json['stock_amount']) or (request.json['price'])) not in [int, float]:
+            return {"Error": "Require int or float type"}
 
         if request.json['product_name'] in [n_product['product_name'] for n_product in products]:
-            product[0]["stock_amount"]+=request.json['stock_amount']
-            return {"Products":product}
-        
-        new_product={"product_id":product_id,
-                     "product_name":product_name,
-                     "category_id":category,
-                     "stock_amount":stock_amount,
-                     "price":price}
-        
+            product[0]["stock_amount"] += request.json['stock_amount']
+            return {"Products": product}, 200  # ok
+
+        new_product = {
+            "product_id": product_id,
+            "product_name": product_name,
+            "category_id": category,
+            "stock_amount": stock_amount,
+            "price": price
+        }
+
         products.append(new_product)
 
-        return {"New Product":new_product},201 #created
+        return {"New Product": new_product}, 201  # created
+
 
 '''Fetch single product'''
+
+
 class ViewSingleProduct(Resource):
-        def get(self,product_id):
-            single_product=[product for product in products if product['product_id']==product_id]
-            if not single_product:
-                return {"Error":"Product Not Found"}
-            return {"Product":single_product},200
+    def get(self, product_id):
+        single_product = [
+            product for product in products if product['product_id'] == product_id]
+        if not single_product:
+            return {"Error": "Product Not Found"}, 400  # Not found
+        return {"Product": single_product}, 200  # ok
+
 
 '''View all sales records'''
+
+
 class ViewSalesRecord(Resource):
     def get(self):
-        return {"Sales Record":sales_record},200
-    
+        return {"Sales Record": sales_record}, 200  # ok
+
     def post(self):
-        current_date=str(date.today())
-        data=request.get_json(force=True)
-        current_product=[product for product in products if product['product_name']==request.json['product_name']]
-        sale_id=len(sales_record)+1
-        attedant_name=data["attedant_name"]
-        product_name=data["product_name"]
-        price=current_product[0]['price']
-        quantity=data["quantity"]
-        total_price=price*quantity
-        date_sold=current_date
+        current_date = str(date.today())
+        data = request.get_json(force=True)
+        current_product = [
+            product for product in products if product['product_name'] == request.json['product_name']]
+        sale_id = len(sales_record)+1
+        attedant_name = data["attedant_name"]
+        product_name = data["product_name"]
+        price = current_product[0]['price']
+        quantity = data["quantity"]
+        total_price = price*quantity
+        date_sold = current_date
 
-        if request.json['product_name'] in  [sale['product_name'] for sale in sales_record]:
-            return {"Message":"{} Exist in cart".format(request.json['product_name'])}
+        if (not request.json or not "product_name" in request.json):
+            return {'Error': "Request Not found"}, 400  # not found
 
+        if request.json['product_name'] in [sale['product_name'] for sale in sales_record]:
+            # ok
+            return {"Message": "{} Exist in cart".format(request.json['product_name'])}, 200
 
-        new_sale={"sale_id":sale_id,
-                  "attedant_name":attedant_name,
-                  "product_name":product_name,
-                  "product_price":price,
-                  "quantity":quantity,
-                  "total_price":total_price,
-                  "date_sold":date_sold}
-        product_sold=[product for product in products if product['product_name']==request.json['product_name']]
-        if not product_sold or product_sold[0]['stock_amount']==0:
-            return {"Message":"{} Out of stock".format(request.json['product_name'])}
+        new_sale = {
+            "sale_id": sale_id,
+            "attedant_name": attedant_name,
+            "product_name": product_name,
+            "product_price": price,
+            "quantity": quantity,
+            "total_price": total_price,
+            "date_sold": date_sold
+        }
+
+        product_sold = [
+            product for product in products if product['product_name'] == request.json['product_name']]
+        if not product_sold or product_sold[0]['stock_amount'] == 0:
+            return {"Message": "{} Out of stock".format(request.json['product_name'])}, 200
         sales_record.append(new_sale)
-        product_sold[0]['stock_amount']-=request.json['quantity']
-        return {"New Sale Record":new_sale},201
+        product_sold[0]['stock_amount'] -= request.json['quantity']
+        return {"New Sale Record": new_sale}, 201  # created
 
 
 '''Fetch single sale record'''
 class SingleSale(Resource):
-    def get(self,sale_id):
-        single_sale=[sale for sale in sales_record if sale['sale_id']==sale_id]
+    def get(self, sale_id):
+        single_sale = [
+            sale for sale in sales_record if sale['sale_id'] == sale_id]
         if not single_sale:
-            return {"Message":"Sale Not Found"}
-        return {"Sale":single_sale},200 #ok
-            
+            return {"Message": "Sale Not Found"}, 400  # ok
+        return {"Sale": single_sale}, 200  # ok
--- app/tests/v1/test_storeviews.py
+++ app/tests/v1/test_storeviews.py
@@ -1,4 +1,4 @@
-#app/tests/v1/test_storeviews
+# app/tests/v1/test_storeviews
 import unittest
 import json
 from app import create_app
@@ -6,81 +6,129 @@ from app import create_app
 '''
 Creating a new testing  class
 '''
+
+
 class TestProducts(unittest.TestCase):
     def setUp(self):
-        self.app=create_app().test_client()
+        self.app = create_app().test_client()
         self.app.testing = True
-        self.products={"product_id":1,
-                       "product_name":"Bread",
-                       "category_id":1,
-                       "stock_amount":2000,
-                       "price":20}
-        self.sales={"sale_id":1,
-                    "attedant_name":"Mary",
-                    "product_name":"Bread",
-                    "product_price":20,
-                    "quantity":3,
-                    "total_price":60,
-                    "date_sold":"12-3-2018"}
-        
+        self.products = {"product_id": 1,
+                         "product_name": "Bread",
+                         "category_id": 1,
+                         "stock_amount": 2000,
+                         "price": 20}
+        self.sales = {"sale_id": 1,
+                      "attedant_name": "Mary",
+                      "product_name": "Bread",
+                      "product_price": 20,
+                      "quantity": 3,
+                      "total_price": 60,
+                      "date_sold": "12-3-2018"}
+
     '''
     Test Get all products
     '''
+
     def test_get_all_products(self):
-        response=self.app.get('/api/v1/products',
-                                headers={'content_type': 'application/json'})
-        self.assertEqual(response.status_code,200)
-    
+        response = self.app.get(
+            '/api/v1/products',
+            headers={'content_type': 'application/json'}
+        )
+        result = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(response.status_code, 200,result['Available Products'])
+
     '''
     Test add new product
     '''
-    def test_add_new_product(self):
-        response=self.app.post('/api/v1/products',
-                               data=json.dumps(self.products),
-                               headers={'content_type': 'application/json'})
 
-        self.assertEqual(response.json,{'New Product': {"category_id": 1,'price': 20,'product_id': 1,'product_name': 'Bread','stock_amount': 2000}}) 
-        self.assertEqual(response.status_code,201)
+    def test_add_new_product(self):
+        response = self.app.post(
+            '/api/v1/products',
+            data=json.dumps(self.products),
+            headers={'content_type': 'application/json'}
+        )
+
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 201, result['New Product'])
 
     '''Test fetch for specific product'''
+
     def test_fetch_single_product(self):
         '''Test fetch for single product [GET request]'''
-        
-        result=self.app.get('/api/v1/products/1',
-                            headers={'content_type': 'application/json'})
-        self.assertEqual(result.status_code,200)
-    
+        response = self.app.get(
+            '/api/v1/products/1',
+            headers={'content_type': 'application/json'}
+        )
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200, result['Product'])
+
     '''
     Test Get all sales
     '''
+
     def test_get_all_sales(self):
-        response=self.app.get('/api/v1/sales',
-                                headers={'content_type': 'application/json'})
-        self.assertEqual(response.status_code,200)
-    
-    
+        response = self.app.get(
+            '/api/v1/sales',
+            headers={'content_type': 'application/json'}
+        )
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200, result['Sales Record'])
+
     '''
     Test add new product
     '''
-    def test_make_new_sale_record(self):
-        response=self.app.post('/api/v1/sales',
-                               data=json.dumps(self.sales),
-                               headers={'content_type': 'application/json'})
 
-        self.assertEqual(response.json,{'New Sale Record':
-                                        {"sale_id":1,
-                                        "attedant_name":"Mary",
-                                        "product_name":"Bread",
-                                        "product_price":20,
-                                        "quantity":3,
-                                        "total_price":60,
-                                        "date_sold":"2018-10-16"}}) 
-        self.assertEqual(response.status_code,201)
+    def test_product_exist(self):
+
+        resp = self.app.get(
+            '/api/v1/sales',
+            headers={'content_type': 'application/json'}
+        )
+        result= json.loads(resp.data.decode('utf-8'))
+        self.assertEqual(resp.status_code, 200, result["Sales Record"])
+
+        response = self.app.post(
+        '/api/v1/sales',
+        data=json.dumps(self.sales),
+        headers={'content_type': 'application/json'}
+        )
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200, result['Message'])
     
+    def test_add_new_sale_record(self):
+        response = self.app.post(
+        '/api/v1/sales',
+        data=json.dumps(self.sales),
+        headers={'content_type': 'application/json'}
+        )
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 201, result['New Sale Record'])
+
     '''Test fetch for specific sale record'''
+
     def test_fetch_single_sale_record(self):
         '''Test fetch for single sale record [GET request]'''
-        
-        result=self.app.get('/api/v1/sales/1',
-                            headers={'content_type': 'application/json'})
-        self.assertEqual(result.status_code,200)
+        resp = self.app.get(
+            '/api/v1/sales/1',
+            headers={'content_type': 'application/json'}
+        )
+        result = json.loads(resp.data.decode('utf-8'))                
+        self.assertEqual(resp.status_code, 200,result['Sale'])
+    
+    '''Test invalid post url'''
+    def test_invalid_post_product_url(self):
+        response = self.app.post(
+            '/api/v1/productss/',
+            data=json.dumps(self.products),
+            headers={'content_type': 'application/json'}
+        )
+        self.assertEqual(response.status, '404 NOT FOUND')
+    
+    def test_invalid_get_product_url(self):
+        response = self.app.get(
+            '/api/v1//productss/',
+            data=json.dumps(self.products),
+            headers={'content_type': 'application/json'}
+        )
+        self.assertEqual(response.status, '404 NOT FOUND')
